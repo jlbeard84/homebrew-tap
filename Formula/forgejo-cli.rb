e1e8634c5a77dd04a5a2e629cd4052b76e4ae1ee
Josh Beard <josh@jlbeard.com>
2026-08-13 09:51:48 -0400

    another fix

--- Formula/forgejo-cli.rb
+++ Formula/forgejo-cli.rb
@@ -9,6 +9,7 @@ class ForgejoCli < Formula
   depends_on "openssl@3"
 
   def install
+    ENV["OPENSSL_DIR"] = formula_opt_prefix("openssl@3")
     system "cargo", "install", *std_cargo_args(path: ".")
 
     generate_completions_from_executable(bin/"fj", "completion")
